fffd24130bf91299b47e6addd21735101737a956
Laura Ramirez <lauramirez0320@gmail.com>
2021-08-06 00:09:55 -0500

    Creation functions fetch- all and main characters

--- src/data.js
+++ src/data.js
@@ -1,9 +1,55 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+import {
+  renderCharacters
+} from './main.js';
+import {
+  renderAllCharacters
+} from "./main.js";
+
+export function dataApi(endpoint) {
+  fetch(endpoint)
+    .then(response => {
+      return response.json();
+    }).then(info => {
+      const list = info.results;
+      list.forEach(element => {
+        renderCharacters(element)
+      });
+
+    }).catch(error => {
+      console.log(error);
+    })
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+
+export function dataApi2(endpoint) {
+  fetch(endpoint)
+    .then(response => {
+      return response.json();
+    }).then(info => {
+      renderAllCharacters(info)
+
+    }).catch(error => {
+      console.log(error);
+    })
 };
+
+// dataApi()
+// const requiredData = require('./data/rickandmorty/rickandmorty.json');
+// console.log(dataRAndM);
+
+// let dataApi = {
+//     allDataRickAndMorty: requiredData,
+//     filterData: function (data, condition){
+//         return this.allDataRickAndMorty.filter()
+//     }
+// }
