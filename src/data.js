3db4ef18a6963ae67432b112094118b62bf6e568
Laura Ramirez <lauramirez0320@gmail.com>
2021-12-29 20:29:07 -0500

    All characters

--- src/data.js
+++ src/data.js
@@ -1,37 +1,21 @@
-// estas funciones son de ejemplo
 
-// export const example = () => {
-//   return 'example';
-// };
+import { renderAllCharacters, renderMainCharacters, loader } from './main.js';
+// import { renderMainCharacters } from "./main.js";
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
 
-import {
-  renderCharacters
-} from './main.js';
-import {
-  renderAllCharacters
-} from "./main.js";
-
-export function dataApi(endpoint) {
+export function dataMainCharacters(endpoint) {
   fetch(endpoint)
     .then(response => {
       return response.json();
     }).then(info => {
-      const list = info.results;
-      list.forEach(element => {
-        renderCharacters(element)
-      });
+      renderMainCharacters(info)
 
     }).catch(error => {
       console.log(error);
     })
 };
 
-
-export function dataApi2(endpoint) {
+export function dataAllCharacters(endpoint) {
   fetch(endpoint)
     .then(response => {
       return response.json();
@@ -43,11 +27,48 @@ export function dataApi2(endpoint) {
     })
 };
 
-// dataApi()
-// const requiredData = require('./data/rickandmorty/rickandmorty.json');
-// console.log(dataRAndM);
+export async function loadAllCharacters(charactersEndPoint, episodesEndPoint) {
+  try {
+    loader();
+    let response = await fetch(charactersEndPoint),
+      dataAllCharacters = await response.json();
+      // console.log(dataAllCharacters);
+      // if (!response.ok) throw {status:response.status, statusText: response.statusText}
+      try {
+        let response = await fetch(episodesEndPoint),
+        dataAllEpisodes = await response.json();
+        // console.log(dataAllEpisodes.results[0].url);
+        renderAllCharacters(dataAllCharacters, dataAllEpisodes);
+      } catch (error) {
+        console.log(error);
+      }
 
-// let dataApi = {
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+  // for (let index = 0; index < dataAllCharacters.results.length; index++) {
+  //   // console.log(dataAllCharacters.results[index], dataAllCharacters.results[index].episode[0]);
+  //   if (dataAllCharacters.results[index].episode[0] == dataAllEpisodes.results[0].url) {
+  //     console.log("True");
+  //   }
+  // }
+  
+}
+
+
+// filterData(data, condition): esta función filter o filtrar recibiría la data,
+//y nos retornaría aquellos datos que sí cumplan con la condición.
+
+// export function filterData(data, condition) {
+//   if (condition == )
+// }
+// sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros.
+//El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+// computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
+
+// let dataAllCharacters = {
 //     allDataRickAndMorty: requiredData,
 //     filterData: function (data, condition){
 //         return this.allDataRickAndMorty.filter()
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import {  dataApi } from './data.js';
-import { dataApi2 } from './data.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { dataAllCharacters } from './data.js';
+import { dataMainCharacters } from './data.js';
+import { loadAllCharacters } from './data.js';
+// import allCharactersData from './allCharactersData/lol/lol.js';
+// import allCharactersData from './allCharactersData/pokemon/pokemon.js';
+// import allCharactersData from './allCharactersData/rickandmorty/rickandmorty.js';
 
 const imagenCharacter = document.querySelector('.card-container');
+const loaderContainer = document.querySelector('.loader-container')
 const form = document.querySelector('.search');
 const inputSearch = document.querySelector('#search');
 const seeMoreButton = document.querySelector('#seeMore-button');
-
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
 const checkAZRadio = document.querySelector('#sortByLetterAZ');
 const checkZARadio = document.querySelector('#sortByLetterZA');
 const checkRadio = document.getElementsByName('sortByLetter')
@@ -19,10 +22,10 @@ const apiUrlEpisode = 'https://rickandmortyapi.com/api/episode'
 
 
 //Search input
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
   console.log(getData());
-  
+
   searching(getData());
 })
 
@@ -40,7 +43,7 @@ function searching(textInput) {
 //   console.log('Click');
 //   const type = 1; 
 //   sortByLetter(type);
-  
+
 // })
 
 // function sortByLetter(type) {
@@ -48,63 +51,32 @@ function searching(textInput) {
 //   let end = 50;
 //   const NewArray = sortData(dataSearch, type);
 //   let dataSearch = NewArray;
-//   dataApi2();
+//   dataMainCharacters();
 // }
 
-//Render characters
-export function renderCharacters(data) {
-    // let imagen = document.createElement("img");
-    // let charachterName = document.createElement("")
-    // imagen.src += ${data.image};
-    // imagen.alt += ${data.name} + "'s picture profile";
-    // imagenCharacter.appendChild(imagen);
-    imagenCharacter.innerHTML += 
-    `<div class="card-wrap">
-      <div class='profile-card'>
-        <div class = 'front'>
-          <img src=${data.image} alt = "${data.name}'s perfil image">
-          <h1>${data.name}</h1>
-        </div>
-        <div class = 'back'>
-          <h2>${data.name}</h2>
-          <p>${data.species} /
-            <img src="/src/img/heart-icon.png" alt="heart-icon">
-            ${data.status}
-          </p>
-          <div class = 'back-info'>
-            <p><span>Gender:</span> ${data.gender}</p>
-            <p><span>Origin:</span> ${data.origin.name}</p>
-            <p><span>Location:</span> ${data.location.name}</p>
-            <p><span>First appearance:</span></p>
-          </div>
-        </div>
-      </div>
-    </div>
- `
-}
 
-//Render all characters
-export function renderAllCharacters(allData) {
-  allData.forEach(data => {
-    
-    imagenCharacter.innerHTML += 
-    `<div class="card-wrap">
+//Render main characters
+export function renderMainCharacters(allData) {
+  console.log(allData);
+  allData.forEach(allCharactersData => {
+    imagenCharacter.innerHTML +=
+      `<div class="card-wrap">
       <div class='profile-card'>
         <div class = 'front'>
-          <img src=${data.image} alt = "${data.name}'s perfil image">
-          <h1>${data.name}</h1>
+          <img src=${allCharactersData.image} alt = "${allCharactersData.name}'s perfil image">
+          <h1>${allCharactersData.name}</h1>
         </div>
         <div class = 'back'>
-          <h2>${data.name}</h2>
-          <p>${data.species} /
+          <h2>${allCharactersData.name}</h2>
+          <p>${allCharactersData.species} /
             <img src="/src/img/heart-icon.png" alt="heart-icon">
-            ${data.status}
+            ${allCharactersData.status}
           </p>
           <div class = 'back-info'>
-            <p><span>Gender:</span> ${data.gender}</p>
-            <p><span>Origin:</span> ${data.origin.name}</p>
-            <p><span>Location:</span> ${data.location.name}</p>
-            <p><span>First appearance:</span></p>
+            <p><span>Gender:</span> ${allCharactersData.gender}</p>
+            <p><span>Origin:</span> ${allCharactersData.origin.name}</p>
+            <p><span>Location:</span> ${allCharactersData.location.name}</p>
+            <p><span>First appearance:</span>${allCharactersData.episode[0]}</p>
           </div>
         </div>
       </div>
@@ -113,12 +85,62 @@ export function renderAllCharacters(allData) {
   });
 }
 
+//Render all characters
+export function renderAllCharacters(allCharactersData, episodes) {
+  // console.log(allCharacterData.results, "allData");
 
-if(location.href == "http://127.0.0.1:5500/index.html"){
-  dataApi2(apiUrl);
-} else if (location.href == "http://127.0.0.1:5500/src/allCharacters.html") {
-  dataApi(apiUrlAllCharacters); 
+  for (let index = 0; index < allCharactersData.results.length; index++) {
+    // console.log(allCharactersData.results[index].name ,allCharactersData.results[index].url,"Episode: " + episodes.results[index].url);
+    if (allCharactersData.results[index].url == episodes.results[index].url) {
+      console.log(allCharactersData.results[index].name);
+      
+    }
+  }
+//   allCharactersData.results.forEach(allCharactersData => {
+//     //   console.log(allCharactersData);
+//     imagenCharacter.innerHTML +=
+//       `<div class="card-wrap">
+//       <div class='profile-card'>
+//         <div class = 'front'>
+//           <img src=${allCharactersData.image} alt = "${allCharactersData.name}'s perfil image">
+//           <h1>${allCharactersData.name}</h1>
+//         </div>
+//         <div class = 'back'>
+//           <h2>${allCharactersData.name}</h2>
+//           <p>${allCharactersData.species} /
+//             <img src="/src/img/heart-icon.png" alt="heart-icon">
+//             ${allCharactersData.status}
+//           </p>
+//           <div class = 'back-info'>
+//             <p><span>Gender:</span> ${allCharactersData.gender}</p>
+//             <p><span>Origin:</span> ${allCharactersData.origin.name}</p>
+//             <p><span>Location:</span> ${allCharactersData.location.name}</p>
+//             <p><span>First appearance:</span></p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+// `
+//   });
 }
+
+export function loader() {
+  return (
+    loaderContainer.innerHTML =
+    `<div class="lds-ripple"><div></div><div></div></div>`
+  )
+}
+
+
+if (location.href == "http://127.0.0.1:5500/index.html") {
+  dataMainCharacters(apiUrl);
+
+} else if (location.href == "http://127.0.0.1:5500/src/pages/allCharacters.html") {
+  // dataAllCharacters(apiUrlAllCharacters);
+
+  loadAllCharacters(apiUrlAllCharacters, apiUrlEpisode)
+}
+// console.log(apiUrlAllCharacters, "apiUrlAllCharacters");
 // seeMoreButton.addEventListener('click', function (event) {
 //   console.log("click");
 // });
